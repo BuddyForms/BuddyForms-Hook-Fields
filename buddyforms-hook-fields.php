0447bb8e0191b199d12b4775537cf37a8251cc81
Sven Lehnert <mail@sven-lehnert.de>
2014-06-15 11:40:15 +0200

    add GPL notice

--- buddyforms-hook-fields.php
+++ buddyforms-hook-fields.php
@@ -8,6 +8,24 @@
  Author URI: http://themekraft.com
  Licence: GPLv3
  Network: false
+
+ *****************************************************************************
+ *
+ * This script is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ****************************************************************************
  */
 
 add_filter('buddyforms_formbuilder_fields_options', 'buddyforms_hook_options_into_formfields', 2, 3);
